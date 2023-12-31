585ad2e0ce1347d2ad0112c5fd411d65e7090152
thill <thill@tntech.edu>
2023-12-31 16:04:21 -0600

    added copy_cloud to crg_segmentation

--- src/crg_segmentation.cpp
+++ src/crg_segmentation.cpp
@@ -3,7 +3,17 @@
 // adapted from example code in tutorial link below 
 // https://pcl.readthedocs.io/projects/tutorials/en/latest/region_growing_rgb_segmentation.html
 
+#include <iostream>
+#include <thread>
+#include <vector>
 #include <Eigen/Core>
+
+#include "ros/package.h"
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <std_msgs/Bool.h>
+
 #include <pcl/common/common.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/passthrough.h>
@@ -11,16 +21,6 @@
 #include <pcl/search/kdtree.h>
 #include <pcl/segmentation/extract_clusters.h>
 #include <pcl_ros/point_cloud.h>
-#include "ros/package.h"
-#include <ros/ros.h>
-#include <visualization_msgs/Marker.h>
-#include <visualization_msgs/MarkerArray.h>
-#include <std_msgs/Bool.h>
-
-#include <iostream>
-#include <thread>
-#include <vector>
-
 #include <pcl/point_types.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/search/search.h>
@@ -29,15 +29,30 @@
 #include <pcl/filters/filter_indices.h> // for pcl::removeNaNFromPointCloud
 #include <pcl/segmentation/region_growing_rgb.h>
 
-
 using namespace std::chrono_literals;
 
 typedef pcl::PointCloud< pcl::PointXYZRGB > PointCloud;
 typedef PointCloud::ConstPtr PointCloudConstPtr;
 typedef PointCloud::Ptr PointCloudPtr;
 
-//typedef pcl::PointIndices::ConstPtr PointIndicesConstPtr;
-//typedef pcl::PointIndices::Ptr PointIndicesPtr;
+typedef pcl::PointIndices::ConstPtr PointIndicesConstPtr;
+typedef pcl::PointIndices::Ptr PointIndicesPtr;
+
+
+// function to copy PointCloud with XYZRGB points
+// it seems like this would be in pcl, but i get a type error with PointXYZRGB
+void copy_cloud(PointCloud &input, PointCloud &output){
+
+  std::cout<<"the point cloud input has "<< input.size()<< " points"<<std::endl;
+
+  for (int i=0; i<input.size(); i++) { // add points to cluster cloud
+    output.push_back(input[i]);  
+  } 
+
+  std::cout<<"the point cloud output has "<< output.size()<< " points"<<std::endl;
+
+}
+
 
 int main(int argc, char** argv)
 {
@@ -111,8 +126,8 @@ int main(int argc, char** argv)
  
   // instantiate some cloud pointers
   PointCloud::Ptr cloud_input (new PointCloud); 
-  PointCloud::Ptr cloud (new PointCloud); // working copy for this routine
-  //PointCloud::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+  PointCloud::Ptr cloud (new PointCloud);      // working copy for this routine
+  PointCloud::Ptr cloud_copy (new PointCloud);
 
   std::cout << "Loading cloud input file:" << input_path << std::endl;
   if (pcl::io::loadPCDFile<pcl::PointXYZRGB> (input_path, *cloud_input) == -1)
@@ -123,7 +138,10 @@ int main(int argc, char** argv)
   std::cout << "Loaded cloud input file: "<< input_path <<std::endl<<
     cloud_input->width * cloud_input->height << " Data points from "<< input_path << std::endl;
  
-  // transform the pointcloud, working copy will be called 'cloud'
+  // make a working copy of the cloud input called 'cloud'
+  copy_cloud(*cloud_input, *cloud);
+
+  // transform the pointcloud
   Eigen::Affine3f transform = Eigen::Affine3f::Identity();
  
   // Define a translation 
@@ -133,8 +151,8 @@ int main(int argc, char** argv)
   transform.rotate (Eigen::AngleAxisf (pre_rotation[1], Eigen::Vector3f::UnitY()));
   transform.rotate (Eigen::AngleAxisf (pre_rotation[2], Eigen::Vector3f::UnitZ()));
 
-  // Execute the transformation, new cloud is called cloud
-  pcl::transformPointCloud (*cloud_input, *cloud, transform); 
+  // Execute the transformation, update the working copy of the cloud
+  pcl::transformPointCloud (*cloud, *cloud, transform); 
 
   std::cout<<"After pre-translation and pre-rotation there are "<<cloud->width * cloud->height << " data points in the point cloud. "<< std::endl;
 
@@ -144,7 +162,7 @@ int main(int argc, char** argv)
                               <<filter_box[2]<<", "<<filter_box[3]<<", "
                               <<filter_box[4]<<", "<<filter_box[5]<<" ]" << std::endl;
 
-  pcl::PassThrough<pcl::PointXYZRGB> pass; //cloud_input
+  pcl::PassThrough<pcl::PointXYZRGB> pass; 
   pass.setInputCloud(cloud);
 
   pass.setFilterFieldName ("x");
@@ -200,7 +218,7 @@ int main(int argc, char** argv)
 
   while (!viewer.wasStopped ())
   {
-    std::this_thread::sleep_for(100us);
+    std::this_thread::sleep_for(100us); // 100us from std::chrono_literals
   }
 
   return (0);
