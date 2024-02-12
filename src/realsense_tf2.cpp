1b454f5b7b66db08c040db4baeea78a633957456
thill <thill@tntech.edu>
2024-02-11 18:34:00 -0600

    fixed merge conflict in realsense_tf, needs testing

--- src/realsense_tf2.cpp
+++ src/realsense_tf2.cpp
@@ -35,6 +35,21 @@ int main(int argc, char** argv){
     ros::Rate r(50);
 
     tf::TransformBroadcaster broadcaster;
+    //static tf2_ros::StaticTransformBroadcaster broadcaster; // the tf2 way
+    // make a 'zero' transform from 'base_link' to 'map' - this is a 'geometry msg'   // zero translation// zero rotation
+    
+    //geometry_msgs::TransformStamped *T_base_map (new geometry_msgs::TransformStamped); // from 'base_link' to 'map'
+    //geometry_msgs::TransformStamped *T_camera_base (new geometry_msgs::TransformStamped); // from 'camera_link' to 'base_link'
+    
+    //T_base_map->header.stamp = ros::Time::now();T_base_map->header.frame_id = "map";T_base_map->child_frame_id ="base_link";
+    //T_base_map->transform.translation.x = 0; T_base_map->transform.translation.y = 0; T_base_map->transform.translation.z = 0;
+    //T_base_map->transform.rotation.x = 0; T_base_map->transform.rotation.y = 0; T_base_map->transform.rotation.z = 0;T_base_map->transform.rotation.w = 1;
+
+    //T_camera_base->header.stamp = ros::Time::now();T_camera_base->header.frame_id = "base_link";T_camera_base->child_frame_id ="camera_link";
+    //T_camera_base->transform.translation.x = 0; T_camera_base->transform.translation.y = 0; T_camera_base->transform.translation.z = 1.0;
+    //T_camera_base->transform.rotation.x = 0; T_camera_base->transform.rotation.y = 0; T_camera_base->transform.rotation.z = 0;T_camera_base->transform.rotation.w = 1;
+
+    //ros::spin();
     float d2r=M_PI/180.0;
     float in2m=0.0254;
     while(n.ok())
@@ -66,7 +81,12 @@ int main(int argc, char** argv){
           tf::Transform(tf::createQuaternionFromRPY(0,0,0), tf::Vector3(0.0, 0.0, 0)),
           ros::Time::now(),"camera_link","camera_color_optical_frame"));
       }
-     
+      //T_base_map->header.stamp = ros::Time::now();  // from the tf2 way
+      //broadcaster.sendTransform(*T_base_map);
+
+      //T_camera_base->header.stamp = ros::Time::now();
+      //broadcaster.sendTransform(*T_camera_base);
+
       r.sleep();
       ros::spinOnce();
     }
